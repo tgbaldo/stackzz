8176b44cca5f065f26471934035a135a7bd21d0d
Tiago Baldo <tiago.baldo@eduzz.com>
2018-08-23 12:11:29 -0300

    ajustes css container

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <section class="content">
+    <section class="content container">
       <div class="row">
         <div class="col-md-12">
           <div class="box box-primary">
