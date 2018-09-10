56aba287b1e32acce01f0d90fbfc844dc9ae861d
Tiago Baldo <tiago.baldo@eduzz.com>
2018-09-10 19:36:13 -0300

    mostra avatar no comentario

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -57,12 +57,12 @@
               <div class="row">
                 <div class="col-md-12">
                   @foreach($post->comments as $c)
-                  <div class="direct-chat-msg">
+                    <div class="direct-chat-msg">
                     <div class="direct-chat-info clearfix">
                       <span class="direct-chat-name pull-left">{{$c->user->name}}</span>
                       <span class="direct-chat-timestamp pull-right">{{date('d/m/Y H:i', strtotime($c->created_at))}}</span>
                     </div>
-                    <img class="direct-chat-img" src="/img/user1-128x128.jpg" alt="Message User Image">
+                    <img class="direct-chat-img" src="{{Auth::user()->avatar}}" alt="Message User Image">
                     <div class="direct-chat-text">
                       {!! $c->content !!}
                     </div>
