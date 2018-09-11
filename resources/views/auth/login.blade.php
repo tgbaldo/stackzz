775159b6b392172bc0971d87c252b6e3d39a9485
Tiago Baldo <tiago.baldo@eduzz.com>
2018-09-11 10:45:21 -0300

    login apenas social

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,39 +8,8 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Faça seu login</p>
-
-    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-        @csrf
-      <div class="form-group has-feedback">
-        <input type="email" name="email" class="form-control" placeholder="E-mail">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        @if ($errors->has('email'))
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-        @endif
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="Senha">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        @if ($errors->has('password'))
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-        @endif
-      </div>
-      <div class="row">
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-4">
-          <a href="{{url('login/google')}}">Login Google</a>
-        </div>
-      </div>
-    </form>
+<!--     <p class="login-box-msg">Faça seu login</p>
+ -->    <a href="{{url('login/google')}}" class="btn btn-primary btn-block">Login com Google</a>
   </div>
   <!-- /.login-box-body -->
 </div>
