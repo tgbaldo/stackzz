9a63daf384fadaadfb960acbea81d9c38a0a055f
Tiago Baldo <tiago.baldo@eduzz.com>
2018-11-01 17:25:26 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ $router->get('/infusion/make', function () {
         'redirectUri'  => 'https://stackzz.herokuapp.com/infusion/callback',
     ));
 
-    $tasks = $infusionsoft->contacts('xml')->all();
+    $tasks = $infusionsoft->contacts('xml');
 
     response()->json($tasks);
 });
