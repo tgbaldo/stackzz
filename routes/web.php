8f504ff1b84ed5d450fdf86d046a0d02e57ad507
Tiago Baldo <tiago.baldo@eduzz.com>
2018-11-01 17:42:50 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ $router->get('/infusion/make', function () {
     $endpoint = $_GET['endpoint'];
 
     $infusionsoft = new \Infusionsoft\Infusionsoft(array(
-        'clientId'     => 'rdh4btg4heh6y7bfy5y9aarz',
+        'clientId'     => 'rdh4btg4heh6y7bfy5y9aarz999999999999999999999999999',
         'clientSecret' => 'GU3a2vcMGV',
         'redirectUri'  => 'https://stackzz.herokuapp.com/infusion/callback',
     ));
